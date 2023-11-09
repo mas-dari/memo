5c3a00c0ace1d2dac69cc687d3f283f26e9d8e41
masdari <masdari.tech@gmail.com>
2023-11-09 12:24:58 +0900

    ローカルストレージへの保存

--- memo/script.js
+++ memo/script.js
@@ -18,19 +18,27 @@ $(".btn_01").on('click', function() {
   });
 
 function addNewNote(text = '') {
+
+//メモを追加する場所を取得
+const main = document.querySelector('main');
+console.log (main,"メモ追加場所")
+
 // div要素を作成
 const note = document.createElement('div');
-console.log(note)
+console.log(note);
+// main.before(note);
 // noteというクラスを追加
 note.classList.add('note');
+
+
 
 // メモ帳を追加
 note.innerHTML = `
 <div class="tools">
-    <button class="edit"><i class="fas fa-edit"></i></button>
-    <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    <input class="placeholder ${text ? "" : "hidden"}"></input>
+    <button class="edit"><i class="fas fa-edit"></i>保存</button>
+    <button class="delete"><i class="fas fa-trash-alt">削除</i></button>
 </div>
-<div class="main ${text ? "" : "hidden"}"></div>
 <textarea class="${text ? "hidden" : ""}"></textarea>
 `
 
@@ -62,31 +70,59 @@ const textArea = note.querySelector('textarea')
 
 
 // テキストエリアのイベント
-textArea.addEventListener('input', (e) => {
-    const { value } = e.target
+// textArea.addEventListener('input', (e) => {
+//     const { value } = e.target
 
     // ローカルストレージの更新
-    updateLS()
-})
+    // updateLS()
+// })
 
-// bodyの子要素として追加
-document.body.appendChild(note)
+//メモを追加する場所を指定
+// main.appendChild(note);
+
+// main.parentNode.insertBefore(note, main.nextSibling);
+// }
+main.insertBefore(note, main.firstChild);
+
+const saveBtn = note.querySelector('.edit');
+$(saveBtn).on("click",function(){
+    const title = $("input").val();
+    const content = $("textarea").val();
+
+localStorage.setItem(title,content);
+
+// const html =`
+//     <li>
+//         <p>${title}<p/>
+//         <p>${title}<p/>
+//     </li>
+// `;
+//     $("#list").append(html);
+});
+
 }
 
 // ローカルストレージにメモ帳を保存
 // ここ理解できていないため、自分で書き換える
-function updateLS() {
-    // 要素を取得
-    const notesText = document.querySelectorAll('textarea')
+
+
+
+
+
+
+
+// function updateLS() {
+//     // 要素を取得
+//     const notesText = document.querySelectorAll('textarea')
  
-    const notes = []
+//     const notes = []
  
-    // 要素を格納
-    notesText.forEach(note => notes.push(note.value))
+//     // 要素を格納
+//     notesText.forEach(note => notes.push(note.value))
  
-    // notesという名前でローカルストレージを保存
-    localStorage.setItem('notes', JSON.stringify(notes))
-}
+//     // notesという名前でローカルストレージを保存
+//     localStorage.setItem('notes', JSON.stringify(notes))
+// }
 
 // メモ帳削除
 // createElementメソッドで作成した要素は、removeメソッドを持っていて削除ができる。
